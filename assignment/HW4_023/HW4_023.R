385e35644f50627d718a5e42e2a459600bc6ed9d
= <=>
2021-10-20 20:19:00 +0700

    update R and README 023

---
+++ assignment/HW4_023/HW4_023.R
@@ -0,0 +1,7 @@
+#install package
+install.packages("dplyr")
+install.packages("ggplot2")
+
+#read data set
+superstore  <- read.csv("https://raw.githubusercontent.com/safesit23/INT214-Statistics/main/datasets/superstore_sales.csv")
+View(superstore)
